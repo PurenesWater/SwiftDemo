6d300579383eec720174d2e2068c6643a4f9d523
Smile <15332481779@163.com>
2020-07-14 09:41:03 +0800

    初始化项目

---
+++ SwiftStatue/SwiftStatue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftStatue
+//
+//  Created by 凌志 on 2020/7/14.
+//  Copyright © 2020 凌志. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
